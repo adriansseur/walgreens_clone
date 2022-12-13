187457fc0468b05e64d9192bbfccf577024e9fb5
Adriansseur <misteralugo@gmail.com>
2022-12-13 02:06:02 -0600

    added snowfall effect from pureSnow.js

--- index.js
+++ index.js
@@ -1,10 +1,87 @@
+let snowflakeCount = 1000
+
 const snowBtn = document.getElementById("snow-btn")
 const heroDiv = document.getElementById('hero')
 
+// variables needed from the code I borrowed
+let bodyHeightPx = document.body.offsetHeight;
+let pageHeightVH = (100 * bodyHeightPx / window.innerHeight);
+
+function spawnSnow(amount) {
+    for (let i = 0; i < amount; i++) {
+        let snowflake = document.createElement('img')
+        snowflake.src = 'media/snowflake.png'
+        snowflake.className = 'snowflake'
+        snowflake.style.right = `${Math.random() * window.innerWidth}px`
+        heroDiv.appendChild(snowflake)
+    }
+}
+
+// Append style for each snowflake to the head
+function addCss(rule) {
+    let css = document.createElement('style');
+    css.type = 'text/css';
+    css.appendChild(document.createTextNode(rule)); // Support for the rest
+    document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+// Math
+function randomInt(value = 100) {
+    return Math.floor(Math.random() * value) + 1;
+}
+
+function randomIntRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+// Create style for snowflake
+function spawnSnowCSS(snowDensity = 200) {
+    let snowflakeName = "snowflake";
+    let rule = ``;
+    // if (typeof baseCss !== 'undefined') {
+    //     rule = baseCss;
+    // }
+
+    for (let i = 1; i < snowDensity; i++) {
+        let randomX = Math.random() * 100; // vw
+        let randomOffset = Math.random() * 10 // vw;
+        let randomXEnd = randomX + randomOffset;
+        let randomXEndYoyo = randomX + (randomOffset / 2);
+        let randomYoyoTime = getRandomArbitrary(0.3, 0.8);
+        let randomYoyoY = randomYoyoTime * pageHeightVH; // vh
+        let randomScale = Math.random();
+        let fallDuration = randomIntRange(10, pageHeightVH / 10 * 3); // s
+        let fallDelay = randomInt(pageHeightVH / 10 * 3) * -1; // s
+        let opacity = Math.random();
+
+        rule += `
+        .${snowflakeName}:nth-child(${i}) {
+            opacity: ${opacity};
+            transform: translate(${randomX}vw, -10px) scale(${randomScale});
+            animation: fall-${i} ${fallDuration}s ${fallDelay}s linear infinite;
+        }
+        @keyframes fall-${i} {
+            ${randomYoyoTime * 100}% {
+                transform: translate(${randomXEnd}vw, ${randomYoyoY}vh) scale(${randomScale});
+            }
+            to {
+                transform: translate(${randomXEndYoyo}vw, ${pageHeightVH}vh) scale(${randomScale});
+            }
+            
+        }
+        `
+    }
+    addCss(rule);
+}
+
+// start snow
 snowBtn.addEventListener("click", function () {
-    let snowflake = document.createElement('img')
-    snowflake.src = 'media/snowflake.png'
-    snowflake.className = 'snowflake'
-    snowflake.style.right = `${Math.random() * window.innerWidth}px`
-    heroDiv.appendChild(snowflake)
+    spawnSnowCSS(snowflakeCount)
+    spawnSnow(snowflakeCount)
 })
